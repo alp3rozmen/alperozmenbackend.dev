cd4a11ddbd58361b8883b68d75d945d78c6fcdcb
alp3rozmen <alp3rozmen@gmail.com>
2025-08-31 21:29:40 +0300

    fix: correct model import for Cars route

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Campaign = require('../models/Campaings');
+const Cars = require('../models/Cars');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
