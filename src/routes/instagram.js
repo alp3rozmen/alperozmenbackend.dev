b2a76fc99917c1f4263ff8576025b0a6b2da6fe5
alp3rozmen <alp3rozmen@gmail.com>
2025-10-15 21:50:54 +0300

    2fa metodları eklendi ama instada skinti var

--- src/routes/instagram.js
+++ src/routes/instagram.js
@@ -4,9 +4,31 @@ const { IgApiClient, IgLoginTwoFactorRequiredError } = require('instagram-privat
 const auth = require('../middleware/auth');
 
 const router = express.Router();
+const client = new IgApiClient();
+let loggedUser = null;
 
-// Bellekte IG instance saklıyoruz (daha iyi yöntem: DB/cache ile user bazlı saklamak)
-let igInstances = {};
+async function TwoFactorLogin(options) {
+  console.log(options);
+  try {
+    loggedUser = await client.account.twoFactorLogin(options)
+  } catch (e) {    
+    throw new Error("Api Hatası : " + e.message);
+  }
+}
+
+async function LoginFnc(pUserName, pPassword) {
+  client.state.generateDevice(pUserName);
+  try {
+    loggedUser = await client.account.login(pUserName, pPassword);
+  } catch (e) {
+    if (e instanceof IgLoginTwoFactorRequiredError) {
+      throw new Error("TwoFactor");
+    }
+    else{
+      throw new Error("Api Hatası : " + e.message);
+    }
+  }
+}
 
 // 🔹 Login endpoint
 router.post('/login', auth, async (req, res) => {
@@ -16,104 +38,62 @@ router.post('/login', auth, async (req, res) => {
     return res.status(400).json({ message: 'Kullanıcı adı ve şifre gerekli.' });
   }
 
-  const ig = new IgApiClient();
-  ig.state.generateDevice(userName);
-  igInstances[userName] = { ig, type: 'login' };
   try {
-    const loggedInUser = await ig.account.login(userName, password);
-
-    if (loggedInUser?.username) {
-      return res.status(200).json({
-        message: '✅ Giriş başarılı.',
-        user: loggedInUser.username,
-      });
+    await LoginFnc(userName, password);
+    if (loggedUser?.username) {
+      return res.status(200).json({ message: 'Giriş Başarılı' , code : 'OK' });
     }
-
-    return res.status(401).json({ message: '❌ Giriş başarısız.' });
-  } catch (err) {
-    
-    if (err instanceof IgLoginTwoFactorRequiredError) {
-      igInstances[userName] = { ig, type: 'two_factor', info: err.twoFactorInfo };
-      return res.status(403).json({
-        message:  err.message +'⚠️ 2FA gerekli. Kullanıcıya kod sorulmalı.',
-        type: 'two_factor',
-        two_factor_info: err.twoFactorInfo,
-      });
+    else{
+      return res.status(200).json({ message: 'Giriş Başarısız' });
     }
-
-    console.error('Login hatası:', err);
-    return res.status(500).json({
-      message: 'Login başarısız.',
-      error: err.message,
-    });
+  } catch (error) {
+    if (error.message == "TwoFactor") {
+      return res.status(403).json({message : 'TwoFactorNeeded'});
+    }
   }
 });
 
-// 🔹 2FA çözme endpoint
-router.post('/login/2fa', auth, async (req, res) => {
-  const { userName, code } = req.body;
+router.post('/2flogin', auth, async (req, res) => {
+  const { pverificationCode, ptwoFactorIdentifier, pusername,pver, pverificationMethod } = req.body;
 
-  if (!userName || !code) {
-    return res.status(400).json({ message: 'Kullanıcı adı ve doğrulama kodu gerekli.' });
+  if (!pusername) {
+    return res.status(400).json({ message: 'Kullanıcı adı gerekli!' });
   }
 
   try {
-    const userSession = igInstances[userName];
-    if (!userSession || userSession.type !== 'two_factor') {
-      return res.status(400).json({ message: 'Önce /login ile 2FA başlatılmalı.' });
+    loggedUser =await TwoFactorLogin({
+      verificationCode: pverificationCode,  
+      twoFactorIdentifier : ptwoFactorIdentifier,
+      username : pusername,
+      trustThisDevice : '1',
+      verificationMethod : pverificationMethod
+    })
+
+    if (loggedUser?.username) {
+      return res.status(200).json({ message: 'Giriş Başarılı' });
     }
-
-    const { ig, info } = userSession;
-
-    const result = await ig.account.twoFactorLogin({
-      verificationCode: code,
-      twoFactorIdentifier: "whatshapp",
-      username: userName,
-      trustThisDevice: '1',
-      verificationMethod: '1', // 1 = SMS, 0 = Authenticator
-    });
-
-    if (result?.username) {
-      delete igInstances[userName];
-      return res.status(200).json({
-        message: '✅ 2FA çözüldü, giriş başarılı.',
-        user: result.username,
-      });
+    else{
+      return res.status(400).json({ message: loggedUser });
     }
-
-    return res.status(401).json({
-      message: '❌ Yanlış 2FA kodu veya giriş başarısız.',
-      result,
-    });
-  } catch (err) {
-    console.error('2FA çözme hatası:', err);
-    return res.status(500).json({
-      message: '2FA çözme başarısız.',
-      error: err.message,
-    });
+  } catch (error) {
+    return res.status(400).json(error.message);
   }
 });
 
 // 🔹 Video paylaşma endpoint
 router.post('/add', auth, async (req, res) => {
   const { caption, videoPath, coverImagePath, userName, password } = req.body;
-
   if (!userName || !password) {
     return res.status(400).json({ message: 'Kullanıcı adı ve şifre gerekli.' });
   }
 
   try {
-    const ig = new IgApiClient();
-    ig.state.generateDevice(userName);
-
-    const loggedInUser = await ig.account.login(userName, password);
-
     if (!loggedInUser?.username) {
       return res.status(401).json({
         message: '❌ Giriş başarısız, video yüklenmedi.',
       });
     }
-
+    
     const bufferVideo = await fs.readFile(videoPath);
     const bufferCoverImage = await fs.readFile(coverImagePath);
 
